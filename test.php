d327cc73e19d89d7be669f9758fb90f8df5dce5c
Ramje Paolo Paras <3907133+ramjeparas@users.noreply.github.com>
2021-11-16 08:15:52 +0800

    bracket

--- test.php
+++ test.php
@@ -1,4 +1,18 @@
 <?php
 echo "testing123 branch 2";
-
+{
+    addslashes
+    {
+        asd;
+    }{
+        {
+            {
+                {
+                    
+                }
+            }
+        }
+    }
+    }
+}
 
